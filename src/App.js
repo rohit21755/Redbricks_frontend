3877b481690bc0c9c499d23c23a5dd613693d8b4
rohit21755 <rohit.k09022002@gmail.com>
2023-08-12 14:39:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Canvas , useThree} from '@react-three/fiber';
+import { useGLTF, Stage, OrbitControls } from '@react-three/drei';
 import './App.css';
+import Model from "./Scene"
+
+function Controls() {
+  const {
+    camera,
+    gl: { domElement },
+  }= useThree();
+
+  return (
+    <OrbitControls
+      enableZoom={true}
+      enablePan={true}
+      enableRotate={true}
+      zoomSpeed={0.5}
+      panSpeed={0.5}
+      rotateSpeed={0.5}
+      target={[0, 0, 0]} // Set the target position for the camera to look at
+      args={[camera, domElement]}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="App-header">
+          <Canvas
+            dpr={[1, 2]}
+            shadows
+            camera={{ fov: 45 }} // Update the camera property name from 'fav' to 'fov'
+            style={{ position: 'absolute' }}
+          >
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Controls/>
+              <Model />
+            
+          </Canvas>
+        </div>
+      </div>
+    </>
   );
 }
 
