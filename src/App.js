ff17fcb004c30b27d1a0319fede6b5a09c9de165
“code-logger” <nitesh6pandey6@gmail.com>
2023-08-12 16:14:01 +0530

    made changes for changing scenes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Canvas , useThree} from '@react-three/fiber';
 import { useGLTF, Stage, OrbitControls } from '@react-three/drei';
 import './App.css';
@@ -25,23 +25,31 @@ function Controls() {
 }
 
 function App() {
+  const [sc,setSc ] = useState('/ss/scene.gltf');
+  // setSc('/skull/scene.gltf')
   return (
     <>
+            <button onClick={()=>{setSc("/skull/scene.gltf")}}>Skull</button>
+          <button onClick={()=>{ setSc("/ss/scene.gltf");console.log("status changed") }}>Scene1</button>
+
       <div className="App">
         <div className="App-header">
+
+
           <Canvas
             dpr={[1, 2]}
             shadows
             camera={{ fov: 45 }} // Update the camera property name from 'fav' to 'fov'
             style={{ position: 'absolute' }}
-          >
+          >      
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <Controls/>
-              <Model />
+              <Model sc={sc}/>
             
           </Canvas>
         </div>
+
       </div>
     </>
   );
